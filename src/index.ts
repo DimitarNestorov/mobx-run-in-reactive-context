8a954f2876e4011ec938fd23329cc45ddeba1f88
Dimitar Nestorov <dimitarnestorov@users.noreply.github.com>
2019-05-27 20:22:24 +0300

    Fixed main value in package.json
    Added typings to package.json
    Added JSDoc example

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,16 @@
 import { autorun } from 'mobx'
 
+/**
+ * Runs `callback` in a reactive context
+ * @example
+ * ```
+ * runInReactiveContext(() => {
+ *   // Read your computed value here
+ *   console.log(object.computedValue)
+ * })
+ * ```
+ * @param callback The function which will be run in a reactive context
+ */
 export default function runInReactiveContext(callback: () => void): void {
 	autorun((reaction) => {
 		callback()
